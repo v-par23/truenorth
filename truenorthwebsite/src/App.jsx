f1bbbc3c65a50fed77cbdcf8a2f499ab873593cd
v-par23 <parikhvedant23@gmail.com>
2025-06-23 12:15:24 -0400

    update layout

--- truenorthwebsite/src/App.jsx
+++ truenorthwebsite/src/App.jsx
@@ -58,13 +58,24 @@ function App() {
       <div >
         <p className = "text-white text-[50px] m-top-50 flex items-center flex-col justify-center">Welcome to Toronto's Premier Table Tennis Destination</p>
         <p className = "text-white text-[25px] m-top-100  flex items-center flex-row justify-center">Home of  <div className = "text-red-400">  Champions.</div> Heart of the<div className = "text-red-400">  North.</div></p>
-        <div className = "w-1/2  m-[5px]">
-            <img src={img} className = "w-[150px] rounded"></img>
-          </div>
-        <div className = "w-[400px] text-white -[20x] m-top 300  flex flex-col items-right">
-          <div>At True North Table Tennis Club, our name represents more than a place. It stands for leadership, resilience, and the spirit of a champion. “True North” is the King of the North, a force powered by discipline, skill, and unwavering passion.<br></br> 
-          We train every player to rise with courage, heart, and relentless dedication, both on and off the table. Welcome to True North, where champions rise and the King of the North is born.</div>
+        <div className="flex-1">
+          <img src={img} alt="True North Table Tennis Club" className="rounded-lg w-w-xl-half h-auto" />
         </div>
+        <div className="flex-1 max-w-xl text-left">
+          <p className="text-white text-[20px] text-lg leading-relaxed">
+                        At True North Table Tennis Club, our name represents more than a place. It stands for leadership, resilience, and the spirit of a champion.
+            “True North” is the King of the North, a force powered by discipline, skill, and unwavering passion.
+            <br /><br />
+            We train every player to rise with courage, heart, and relentless dedication, both on and off the table.
+            Welcome to True North, where champions rise and the King of the North is born.
+          </p>
+        </div>
+        <div className="text-white text-[25px] flex flex-wrap justify-center items-center gap-8 mt-16 text-center text-xl">
+        <div className="border-t-2 border-red-500 pt-2">Elite Coaching Team</div>
+        <div className="border-t-2 border-red-500 pt-2">State-of-the-Art Facilities</div>
+        <div className="border-t-2 border-red-500 pt-2">Tailored Programs for All Levels</div>
+        <div className="border-t-2 border-red-500 pt-2">Community and Competition</div>
+      </div>
         
       </div>
     );

e3aa55cec4ab40879fcc03d936e5e609c29e3a22
v-par23 <parikhvedant23@gmail.com>
2025-06-23 13:58:12 -0400

    image scale + coaches

--- truenorthwebsite/src/App.jsx
+++ truenorthwebsite/src/App.jsx
@@ -29,14 +29,6 @@ function App() {
     );
   }
 
-  // const Intro = () => {
-  //   return(
-  //     <div>
-  //       <h1>True North Table Tennis</h1>
-  //     </div>
-  //   );
-  // }
-
   const AboutUs = () => {
     return(
       <div className = " text-red-700 w-[1500px] h-dvh flex items-center flex-col justify-center">
@@ -58,8 +50,8 @@ function App() {
       <div >
         <p className = "text-white text-[50px] m-top-50 flex items-center flex-col justify-center">Welcome to Toronto's Premier Table Tennis Destination</p>
         <p className = "text-white text-[25px] m-top-100  flex items-center flex-row justify-center">Home of  <div className = "text-red-400">  Champions.</div> Heart of the<div className = "text-red-400">  North.</div></p>
-        <div className="flex-1">
-          <img src={img} alt="True North Table Tennis Club" className="rounded-lg w-w-xl-half h-auto" />
+        <div className="flex-1 max-w-xl right" >
+          <img src={img} alt="True North Table Tennis Club" className="rounded-lg w-w-xl" />
         </div>
         <div className="flex-1 max-w-xl text-left">
           <p className="text-white text-[20px] text-lg leading-relaxed">
@@ -80,12 +72,41 @@ function App() {
       </div>
     );
   }
+
+  const Coaches = () => {
+    return( 
+      <div >
+        <p className = "text-white text-[50px] m-top-50 flex items-center flex-col justify-center">Welcome to Toronto's Premier Table Tennis Destination</p>
+        <p className = "text-white text-[25px] m-top-100  flex items-center flex-row justify-center">Home of  <div className = "text-red-400">  Champions.</div> Heart of the<div className = "text-red-400">  North.</div></p>
+        <div className="flex-1 max-w-xl right" >
+          <img src={img} alt="True North Table Tennis Club" className="rounded-lg w-w-xl" />
+        </div>
+        <div className="flex-1 max-w-xl text-left">
+          <p className="text-white text-[20px] text-lg leading-relaxed">
+                        At True North Table Tennis Club, our name represents more than a place. It stands for leadership, resilience, and the spirit of a champion.
+            “True North” is the King of the North, a force powered by discipline, skill, and unwavering passion.
+            <br /><br />
+            We train every player to rise with courage, heart, and relentless dedication, both on and off the table.
+            Welcome to True North, where champions rise and the King of the North is born.
+          </p>
+        </div>
+        <div className="text-white text-[25px] flex flex-wrap justify-center items-center gap-8 mt-16 text-center text-xl">
+        <div className="border-t-2 border-red-500 pt-2">Elite Coaching Team</div>
+        <div className="border-t-2 border-red-500 pt-2">State-of-the-Art Facilities</div>
+        <div className="border-t-2 border-red-500 pt-2">Tailored Programs for All Levels</div>
+        <div className="border-t-2 border-red-500 pt-2">Community and Competition</div>
+      </div>
+        
+      </div>
+    );
+  }
+
   return (
     <div className = " bg-black w-dvw h-dvh flex flex-col items-center">
       <Navbar/>
          <Router>
           <Routes>
-            <Route path="/" element={<Intro/>} />
+            <Route path="/" element={<Coaches/>} />
             {/* <Route path="/about" element={} />
             <Route path="/contact" element={} /> */}
           </Routes>
